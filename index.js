86f2acaaf614aed41684210fb480f460d87ce2a0
Pepito94510 <kevin.boissiere94@gmail.com>
2023-09-06 10:38:59 +0200

    add test route

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.status(200).send('Hello world !');
 });
 
+app.get('/test', (req, res) => {
+    res.status(200).send('ça marche bien !');
+});
+
 app.listen(port, () => {
     console.log('Server listen on http://localhost:5001');
 });
