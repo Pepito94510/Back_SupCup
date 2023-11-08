13bc933e84429d6cd298d57934869a9e295cef7a
Pepito94510 <kevin.boissiere94@gmail.com>
2023-11-08 16:02:08 +0100

    swagger role API

--- app/routes/role.js
+++ app/routes/role.js
@@ -3,17 +3,68 @@ var router = express.Router();
 
 const role = require('../models/role');
 
+/**
+ * @swagger
+ * 
+ *  components:
+ *      schema:
+ *          role:
+ *              type: object
+ *              properties: 
+ *                  name:
+ *                      type: string
+ */
+
 router.get('/', async(req, res) => {
     let roles = await role.findAll();
     res.status(200).json(roles);
 });
 
+/**
+ * @swagger
+ * /role/{roleId}:
+ *  get:
+ *      tags: 
+ *          - Role
+ *      description: Retourne les informations d'un role en fonction de son id
+ *      parameters: 
+ *          - in: path
+ *            name: roleId
+ *            description: id du role
+ *      responses: 
+ *          200:
+ *              description: Retourne les informations d'un role
+ *          404:
+ *              description: L'id role saisie n'est pas connu ne base de données
+ */
 router.get('/:roleId', async(req, res) => {
     let { roleId } = req.params;
     let aRole = await role.findByPk(roleId);
     res.status(200).json(aRole);
 });
 
+/**
+ * @swagger
+ * /role/create/{roleId}:
+ *  post:
+ *      tags: 
+ *          - Role
+ *      description: Retourne les informations d'un role en fonction de son id
+ *      requestBody: 
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#components/schema/role'
+ *      parameters: 
+ *          - in: path
+ *            name: roleId
+ *            description: id du role
+ *      responses: 
+ *          200:
+ *              description: Retourne les informations d'un role
+ *          404:
+ *              description: L'id role saisie n'est pas connu ne base de données
+ */
 router.post('/create', async(req, res) => {
     console.log(req.body);
 
@@ -26,7 +77,29 @@ router.post('/create', async(req, res) => {
     res.send("Role_created");
 });
 
-router.post('/update/:roleId', async(req, res) => {
+/**
+ * @swagger
+ * /role/update/{roleId}:
+ *  put:
+ *      tags: 
+ *          - Role
+ *      description: Modifie les informations d'un role en fonction de son id
+ *      requestBody: 
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#components/schema/role'
+ *      parameters: 
+ *          - in: path
+ *            name: roleId
+ *            description: id du role
+ *      responses: 
+ *          200:
+ *              description: Modifie les informations d'un role
+ *          404:
+ *              description: L'id role saisie n'est pas connu ne base de données
+ */
+router.put('/update/:roleId', async(req, res) => {
     let { roleId } = req.params;
     let aRole = await role.findByPk(roleId);
 
@@ -39,7 +112,24 @@ router.post('/update/:roleId', async(req, res) => {
     res.send("Role_updated");
 });
 
-router.post('/delete/:roleId', async(req, res) => {
+/**
+ * @swagger
+ * /role/delete/{roleId}:
+ *  delete:
+ *      tags: 
+ *          - Role
+ *      description: Supprime un role en fonction de son id
+ *      parameters: 
+ *          - in: path
+ *            name: roleId
+ *            description: id du role
+ *      responses: 
+ *          200:
+ *              description: Supprime un role
+ *          404:
+ *              description: L'id role saisie n'est pas connu ne base de données
+ */
+router.delete('/delete/:roleId', async(req, res) => {
     let { roleId } = req.params;
     let aRole = await role.findByPk(roleId);
 
